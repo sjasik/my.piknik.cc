7e11b30b8a8b7f0cf04b0468eb623b4c50802968
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2020-11-26 15:14:17 +0100

    initial

---
+++ precache-manifest.eceffc480618d840560c51281e486d95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d111fa1c4a98e18625c",
+    "url": "/css/app.69c743c8.css"
+  },
+  {
+    "revision": "5d111fa1c4a98e18625c",
+    "url": "/js/app.a82d5bd8.js"
+  },
+  {
+    "revision": "813e9b9eb16b4e89dece",
+    "url": "/css/chunk-vendors.5579511a.css"
+  },
+  {
+    "revision": "813e9b9eb16b4e89dece",
+    "url": "/js/chunk-vendors.c3e4b683.js"
+  },
+  {
+    "revision": "5e9f2fda6efed953473d6a07ca69cb98",
+    "url": "/img/piknik-logo-white.5e9f2fda.svg"
+  },
+  {
+    "revision": "7b92b3832466be098d8dd181ffccb5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
