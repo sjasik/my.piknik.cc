e9d4994a5ed5c90cb9c4d1bdc0b79b145238aa61
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2021-01-07 13:52:44 +0100

    dish options, delivery zones, online payments

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c99c5b75186a2059e82ababbcfde1fa.js"
+  "/precache-manifest.6296dfdf2dc774578f265fdf15f2d70a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
