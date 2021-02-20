037637513750e072267a0f72ec281388cc616e91
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2021-02-20 16:35:50 +0100

    restaurantId bug on login fixed

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5427c3d6e174fcceb0be05865fd8e0dd.js"
+  "/precache-manifest.57230204c1ef72371b084199007a40d5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
