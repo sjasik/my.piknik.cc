ec416fbd25903dd3422d65b2395382c646bd3b28
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2021-06-05 18:23:52 +0200

    Chart height fixed

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b570578afdafd5636053c3cbe58030a.js"
+  "/precache-manifest.3c68499ce951ace1fb35e1be0d346768.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
