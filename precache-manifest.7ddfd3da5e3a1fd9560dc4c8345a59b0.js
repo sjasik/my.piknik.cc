6f23a394b7f21d627851ffd11acbdf3faefee73c
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2021-06-05 18:30:15 +0200

    Chart height fixed

---
+++ precache-manifest.7ddfd3da5e3a1fd9560dc4c8345a59b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6382558a319b662166fc",
+    "url": "/css/app.7e6d1b3a.css"
+  },
+  {
+    "revision": "6382558a319b662166fc",
+    "url": "/js/app.0c98da4b.js"
+  },
+  {
+    "revision": "6a1b5a0acea6ad0db69d",
+    "url": "/css/chunk-vendors.c72caaa8.css"
+  },
+  {
+    "revision": "6a1b5a0acea6ad0db69d",
+    "url": "/js/chunk-vendors.9a155005.js"
+  },
+  {
+    "revision": "df13bab8cdfb67b2de207fe97b1e43c8",
+    "url": "/media/notification.df13bab8.mp3"
+  },
+  {
+    "revision": "5e9f2fda6efed953473d6a07ca69cb98",
+    "url": "/img/piknik-logo-white.5e9f2fda.svg"
+  },
+  {
+    "revision": "c86993dfafbb52756a13b82283233ed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c68499ce951ace1fb35e1be0d346768.js"
+  "/precache-manifest.7ddfd3da5e3a1fd9560dc4c8345a59b0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
