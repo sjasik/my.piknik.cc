779b9c28618b6335435f596cfc18a897cd4f57a0
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2021-12-06 13:12:24 +0100

    Order sse on error fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7941674d7b9317a024999a39bcb9458d.js"
+  "/precache-manifest.b42a729696eddc8ca790bfee7e76bee9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
