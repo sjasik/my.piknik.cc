dbc6108ec5b93dfbdd0effa688b73f37d289bfbb
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2022-03-20 19:13:04 +0100

    Adding cookie consent

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2e8e5de27a1abb5d372a8910eee17b6.js"
+  "/precache-manifest.a9a426ba5a906ff4378b4b8c6d8b1607.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
